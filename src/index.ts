010d48ac0a2db27e8b681f408c2fb5e07e8bd0d1
Mohammed Ziyan <nazzz.infotech@gmail.com>
2026-03-07 21:03:33 +0530

    shapes export added

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,29 @@
+import Circle from "./components/shapes/Circle";
+import Heart from "./components/shapes/Heart";
+import Hexagon from "./components/shapes/Hexagon";
+import Lollipop from "./components/shapes/Lollipop";
+import Octagon from "./components/shapes/Octagon";
+import Rectangle from "./components/shapes/Rectangle";
+import Rhombus from "./components/shapes/Rhombus";
 import "./index.css";
+import StarBurst from "./components/shapes/StarBurst";
+import Triangle from "./components/shapes/Triangle";
+import Star from "./components/shapes/Star";
+import ZigzagBox from "./components/shapes/zigzag/ZigzagBox";
+import ZigzagLine from "./components/shapes/zigzag/ZigzagLine";
 
 export { StoreRenderer } from "./StoreRenderer";
+export const Shapes = {
+  Circle,
+  Heart,
+  Hexagon,
+  Lollipop,
+  Octagon,
+  Rectangle,
+  Rhombus,
+  Star,
+  StarBurst,
+  Triangle,
+  ZigzagBox,
+  ZigzagLine,
+};
